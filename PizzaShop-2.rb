8065130cfb26b8cda32c1db41350c598f29c1ea2
Oleg-rb <oleg1107@rambler.ru>
2020-04-02 11:25:30 +0300

    Add model

--- PizzaShop-2.rb
+++ PizzaShop-2.rb
@@ -3,6 +3,12 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:pizzashop-2.db"
+
+class Product < ActiveRecord::Base
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/Oleg-rb/Template/\">Original</a> pattern has been modified for <a href=\"https://github.com/Oleg-rb/Template/\">Template</a>"			
