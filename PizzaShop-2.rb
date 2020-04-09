4d36152af03071081624b257d2d7e03491456277
Oleg-rb <oleg1107@rambler.ru>
2020-04-09 17:37:52 +0300

    Add Orders model + migration

--- PizzaShop-2.rb
+++ PizzaShop-2.rb
@@ -10,6 +10,9 @@ set :database, "sqlite3:pizzashop-2.db"
 class Product < ActiveRecord::Base
 end
 
+class Order < ActiveRecord::Base
+end
+
 get '/' do
 
 	@products = Product.all
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_084635) do
+ActiveRecord::Schema.define(version: 2020_04_09_141223) do
+
+  create_table "orders", force: :cascade do |t|
+    t.text "orders_input"
+    t.text "name"
+    t.text "phone"
+    t.text "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
